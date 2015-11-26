498317137d371794aa9185b92f277276af3aabca
AnyaAlves <anabeatrizdsialves@gmail.com>
2015-11-26 19:04:37 +0000

    debug file explorer

--- SecureSMS/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/securesms/presentation/MainActivity.java
+++ SecureSMS/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/securesms/presentation/MainActivity.java
@@ -1,27 +1,45 @@
 package pt.ulisboa.tecnico.meic.sirs.securesms.presentation;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.content.Intent;
+import android.widget.Toast;
 
+import com.rustamg.filedialogs.FileDialog;
+import com.rustamg.filedialogs.OpenFileDialog;
+
+import java.io.File;
 import java.security.Security;
 import java.util.ArrayList;
 
 import pt.ulisboa.tecnico.meic.sirs.securesms.domain.SmsMessage;
 import pt.ulisboa.tecnico.meic.sirs.securesms.R;
+import pt.ulisboa.tecnico.meic.sirs.securesms.service.ImportCACertificateService;
+import pt.ulisboa.tecnico.meic.sirs.securesms.service.ImportPrivateKeysService;
+import pt.ulisboa.tecnico.meic.sirs.securesms.service.ImportUserCertificateService;
+import pt.ulisboa.tecnico.meic.sirs.securesms.service.exceptions.SecureSMSException;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FileDialog.OnFileSelectedListener{
     boolean firstUse = true;
     private FloatingActionButton fabPlus;
     private FloatingActionButton fabcomposeMessage;
@@ -83,8 +101,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void configureApplication(View view) {
-        SetupApplicationActivity configureApplication = new SetupApplicationActivity(this);
-        configureApplication.display();
+        setupApplicationActivity();
+        display();
+        //SetupApplicationActivity configureApplication = new SetupApplicationActivity(this);
+        //configureApplication.display();
     }
 
     private void showInbox() {
@@ -135,5 +155,168 @@ public class MainActivity extends AppCompatActivity {
         fabAddContact.setVisibility(View.INVISIBLE);
         Intent addContactIntent = new Intent(getApplicationContext(), AddContactActivity.class);
         startActivity(addContactIntent);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    ///SETUP APPLICATION
+
+    public enum ImportFileType {
+        CA_CERTIFICATE,
+        USER_CERTIFICATE,
+        PRIVATE_KEY
+    }
+
+    View view;
+    Spinner spCACertificate;
+    Spinner spUserCertificate;
+    Spinner spPrivateKeys;
+    EditText etPassword;
+
+    public void setupApplicationActivity() {
+        //show another view without exiting the current
+        LayoutInflater factory = LayoutInflater.from(this);
+        view = factory.inflate(R.layout.activity_setup_application, null);
+
+        //set items
+        spCACertificate = (Spinner) findViewById(R.id.spCACertificate);
+        spCACertificate.setOnItemSelectedListener(customOnItemSelectedListener(ImportFileType.CA_CERTIFICATE));
+
+        spUserCertificate = (Spinner) findViewById(R.id.spUserCertificate);
+        spUserCertificate.setOnItemSelectedListener(customOnItemSelectedListener(ImportFileType.USER_CERTIFICATE));
+
+        spPrivateKeys = (Spinner) findViewById(R.id.spPrivateKeys);
+        spPrivateKeys.setOnItemSelectedListener(customOnItemSelectedListener(ImportFileType.PRIVATE_KEY));
+
+        etPassword = (EditText) findViewById(R.id.etPassword);
+    }
+
+    public AdapterView.OnItemSelectedListener customOnItemSelectedListener(final ImportFileType type) {
+        return new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (!parent.getSelectedItem().toString().equals("None")) {
+                    showFileDialog(".pem", type);
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+    }
+
+    public void showFileDialog(String extension, ImportFileType type) {
+        FileDialog openDialog = new OpenFileDialog();
+        Bundle args = new Bundle();
+        args.putString(FileDialog.EXTENSION, extension);
+        args.putInt("type", type.ordinal());
+        openDialog.setArguments(args);
+        openDialog.setStyle(android.support.v4.app.DialogFragment.STYLE_NO_TITLE, R.style.AppTheme);
+        openDialog.show(getSupportFragmentManager(), OpenFileDialog.class.getName());
+    }
+
+
+    @Override
+    public void onFileSelected(FileDialog dialog, File file) {
+        finish();
+        ImportFileType type = ImportFileType.values()[dialog.getArguments().getInt("type")];
+        try {
+            switch (type) {
+                case CA_CERTIFICATE: {
+                    ImportCACertificateService service = new ImportCACertificateService(file.getPath(), "storagePassword");
+                    service.Execute();
+                    break;
+                }
+                case USER_CERTIFICATE: {
+                    ImportUserCertificateService service = new ImportUserCertificateService(file.getPath(), true, "storagePassword");
+                    service.Execute();
+                    if (!service.getResult()) {
+                        //prompt user to accept
+                        service = new ImportUserCertificateService(file.getPath(), false, "password");
+                        service.Execute();
+                    }
+                    break;
+                }
+                case PRIVATE_KEY: {
+                    ImportPrivateKeysService service = new ImportPrivateKeysService(file.getPath(), "password12345", "storagePassword");
+                    service.Execute();
+                    break;
+                }
+            }
+            Toast.makeText(getApplicationContext(), "Import successful", Toast.LENGTH_SHORT);
+        } catch (SecureSMSException exception) {
+            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT);
+        }
+
+    }
+
+    public void createDialogItems() {
+        //drop down
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("None");
+        options.add("Choose from file");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, options);
+
+        spCACertificate.setAdapter(adapter);
+        spUserCertificate.setAdapter(adapter);
+        spPrivateKeys.setAdapter(adapter);
+
+        //show password
+        CheckBox cbShowPassword = (CheckBox) view.findViewById(R.id.cbShowPassword);
+        cbShowPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    etPassword.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    etPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+                etPassword.setSelection(etPassword.getText().length());
+            }
+        });
+    }
+
+    public void updateSpinnerOption(Spinner sp, String filename) {
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("None");
+        options.add(filename);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, options);
+    }
+
+    public void display() {
+        createDialogItems();
+
+        //create alert dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(view);
+        //create buttons
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                //check empty input
+                //ImportCACertificateService
+                //ImportUserCertificateService
+                //ImportPrivateKeysService
+                //DefinePasswordService
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
     }
 }
