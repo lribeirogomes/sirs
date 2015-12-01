49f7334e4cbda2c54a72da3965d92e102017682c
AnyaAlves <anabeatrizdsialves@gmail.com>
2015-12-01 14:56:14 +0000

    import certificates integrated

--- SecureSMS/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/securesms/presentation/AddContactActivity.java
+++ SecureSMS/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/securesms/presentation/AddContactActivity.java
@@ -4,21 +4,34 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import com.rustamg.filedialogs.FileDialog;
+import com.rustamg.filedialogs.OpenFileDialog;
+
+import java.io.File;
 
 import pt.ulisboa.tecnico.meic.sirs.securesms.R;
 import pt.ulisboa.tecnico.meic.sirs.securesms.service.AddContactService;
+import pt.ulisboa.tecnico.meic.sirs.securesms.service.ImportContactCertificateService;
+import pt.ulisboa.tecnico.meic.sirs.securesms.service.ImportUserCertificateService;
 import pt.ulisboa.tecnico.meic.sirs.securesms.service.exceptions.FailedServiceException;
+import pt.ulisboa.tecnico.meic.sirs.securesms.service.exceptions.SecureSmsException;
 
-public class AddContactActivity extends AppCompatActivity {
+public class AddContactActivity extends AppCompatActivity implements FileDialog.OnFileSelectedListener {
 
     private EditText etContactName;
     private EditText etContactPhoneNumber;
+    private EditText etContactCertificate;
+    private CheckBox cbSelfSignedContact;
+    private TextView tvContactCertificateValid;
+    private TextView tvContactCertificateInvalid;
+    private ImageButton bContactCertificate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +43,13 @@ public class AddContactActivity extends AppCompatActivity {
 
         etContactName = (EditText) findViewById(R.id.etContactName);
         etContactPhoneNumber = (EditText) findViewById(R.id.etContactPhoneNumber);
-
-        Spinner spCertificate = (Spinner) findViewById(R.id.spCertificate);
-        //drop down
-        ArrayList<String> options = new ArrayList<String>();
-        options.add("None");
-        options.add("Choose from file");
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_dropdown_item, options);
-
-        spCertificate.setAdapter(adapter);
+        etContactCertificate = (EditText) findViewById(R.id.etContactCertificate);
+        etContactCertificate.setKeyListener(null);
+        bContactCertificate = (ImageButton) findViewById(R.id.bContactCertificate);
+        bContactCertificate.setOnClickListener(customOnClickListener());
+        cbSelfSignedContact = (CheckBox) findViewById(R.id.cbSelfSignedContact);
+        tvContactCertificateValid = (TextView) findViewById(R.id.tvContactCertificateValid);
+        tvContactCertificateInvalid = (TextView) findViewById(R.id.tvContactCertificateInvalid);
     }
 
     public void confirmAddContact(View view) {
@@ -53,11 +63,55 @@ public class AddContactActivity extends AppCompatActivity {
             service.execute();
             finish();
             toast = Toast.makeText(getApplicationContext(), "Contact added successfully.", Toast.LENGTH_SHORT);
-        }
-        catch(FailedServiceException exception) {
+        } catch (FailedServiceException exception) {
             //should be dialog or snackbar
             toast = Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT);
         }
         toast.show();
+    }
+
+    //file explorer stuff
+
+    @Override
+    public void onFileSelected(FileDialog dialog, File file) {
+        try {
+            ImportContactCertificateService service = new ImportContactCertificateService(file.getPath(), true);
+            service.execute();
+            if (!service.getResult()) {
+                //prompt user to accept
+                service = new ImportContactCertificateService(file.getPath(), false);
+                service.execute();
+                tvContactCertificateValid.setVisibility(View.INVISIBLE);
+                tvContactCertificateInvalid.setVisibility(View.VISIBLE);
+                //tvUserCertificateSelfSigned.setVisibility(View.VISIBLE);
+            } else {
+                tvContactCertificateValid.setVisibility(View.VISIBLE);
+                tvContactCertificateInvalid.setVisibility(View.INVISIBLE);
+                //tvUserCertificateSelfSigned.setVisibility(View.INVISIBLE);
+            }
+        } catch (SecureSmsException exception) {
+            tvContactCertificateValid.setVisibility(View.INVISIBLE);
+            tvContactCertificateInvalid.setVisibility(View.VISIBLE);
+        }
+        etContactCertificate.setText(file.getPath());
+    }
+
+    //custom listener
+    private View.OnClickListener customOnClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileDialog(".pem");
+            }
+        };
+    }
+
+    public void showFileDialog(String extension) {
+        FileDialog openDialog = new OpenFileDialog();
+        Bundle args = new Bundle();
+        args.putString(FileDialog.EXTENSION, extension);
+        openDialog.setArguments(args);
+        openDialog.setStyle(android.support.v4.app.DialogFragment.STYLE_NO_TITLE, R.style.AppTheme);
+        openDialog.show(getSupportFragmentManager(), OpenFileDialog.class.getName());
     }
 }
--- SecureSMS/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/securesms/service/ImportContactCertificateService.java
+++ SecureSMS/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/securesms/service/ImportContactCertificateService.java
@@ -2,36 +2,47 @@ package pt.ulisboa.tecnico.meic.sirs.securesms.service;
 
 import java.security.cert.CertPathValidatorException;
 
+import pt.ulisboa.tecnico.meic.sirs.securesms.dataAccess.DataManager;
 import pt.ulisboa.tecnico.meic.sirs.securesms.dataAccess.KeyManager;
+import pt.ulisboa.tecnico.meic.sirs.securesms.dataAccess.UserManager;
 import pt.ulisboa.tecnico.meic.sirs.securesms.dataAccess.exceptions.FailedToLoadKeyStoreException;
 import pt.ulisboa.tecnico.meic.sirs.securesms.dataAccess.exceptions.FailedToRetrieveKeyException;
 import pt.ulisboa.tecnico.meic.sirs.securesms.dataAccess.exceptions.FailedToStoreException;
 import pt.ulisboa.tecnico.meic.sirs.securesms.dataAccess.exceptions.InvalidCertificateException;
+import pt.ulisboa.tecnico.meic.sirs.securesms.dataAccess.exceptions.KeyStoreIsLockedException;
+import pt.ulisboa.tecnico.meic.sirs.securesms.domain.User;
+import pt.ulisboa.tecnico.meic.sirs.securesms.domain.exceptions.FailedToRetrieveUserException;
 import pt.ulisboa.tecnico.meic.sirs.securesms.service.exceptions.FailedServiceException;
 
 /**
  * Created by joao on 19/11/15.
  */
 public class ImportContactCertificateService extends SecureSmsService {
-    private String _filename, _storagePassword;
+    private String _filename;
     private boolean _validate;
+    private boolean _isValid;
 
-    public ImportContactCertificateService(String filename, boolean validate, String storagePassword) {
+    public ImportContactCertificateService(String filename, boolean validate) {
         _filename = filename;
-        _storagePassword = storagePassword;
         _validate = validate;
     }
 
     public void execute() throws FailedServiceException {
         try {
-            KeyManager km = KeyManager.getInstance(_storagePassword);
+            KeyManager km = KeyManager.getInstance();
             km.importUserCertificates(_filename, false, _validate);
-        } catch ( FailedToLoadKeyStoreException
+        } catch ( KeyStoreIsLockedException
                 | FailedToStoreException
                 | InvalidCertificateException
-                | FailedToRetrieveKeyException
-                | CertPathValidatorException exception) {
+                | FailedToRetrieveKeyException exception) {
             throw new FailedServiceException("import contact certificate", exception);
+        } catch (CertPathValidatorException e) {
+            _isValid = false;
         }
+        _isValid = true;
+    }
+
+    public boolean getResult() {
+        return _isValid;
     }
 }
