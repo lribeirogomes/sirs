66eca928433d760c590ac79a6e2789b0a9192634
AnyaAlves <anabeatrizdsialves@gmail.com>
2015-12-02 19:58:53 +0000

    receiver passes data to another intent

--- SecureSMS/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/securesms/presentation/SmsReceiver.java
+++ SecureSMS/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/securesms/presentation/SmsReceiver.java
@@ -28,16 +28,15 @@ import pt.ulisboa.tecnico.meic.sirs.securesms.service.ReceiveSmsMessageService;
  * Created by joao on 11/1/15.
  */
 public class SmsReceiver extends BroadcastReceiver {
-    private String _senderAddress = "";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
         Bundle bundle = intent.getExtras();
         Object[] smsExtra = (Object[]) bundle.get("pdus");
 
 
         //Get the date from the SMS
+        String address = "";
         byte[] completeData = {};
         android.telephony.SmsMessage androidSms;
         for (int i = 0; i < smsExtra.length; i++) {
@@ -47,63 +46,13 @@ public class SmsReceiver extends BroadcastReceiver {
             if (data != null) {
                 completeData = Arrays.concatenate(completeData, data);
             }
-            _senderAddress = androidSms.getOriginatingAddress();
+            address = androidSms.getOriginatingAddress();
         }
 
-        //Figure out where to deliver it
-        try {
-            ReceiveSmsMessageService service = new ReceiveSmsMessageService(_senderAddress, completeData);
-            service.execute();
-            //TEST CODE
-
-            showNotification(context, "You have a new request", "Click here for more details", true);
-
-
-            //TEST CODE
-
-
-      /*      if (completeData[0] == SmsMessageType.Text.ordinal()) {
-                SmsMessage sms = service.getResult();
-                String contactName = sms.getContact().getName();
-                showNotification(context, contactName, sms.getContent(), false);
-            }else {
-                showNotification(context, "You have a new request", "Click here for more details", true);
-            }*/
-
-        } catch (Exception exception) {
-            Toast.makeText(context, exception.getMessage(), Toast.LENGTH_SHORT);
-        }
-
+        Intent result = new Intent(context, ReceiveSmsActivity.class);
+        result.putExtra(ReceiveSmsActivity.ADDRESS, address);
+        result.putExtra(ReceiveSmsActivity.DATA, completeData);
+        result.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(result);
     }
-
-
-
-    public void showNotification(Context context, String title, String subtext, boolean showAckDialog) {
-        // Sets an ID for the notification, so it can be updated
-        int notifyID = 1;
-
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(context)
-                        .setSmallIcon(R.drawable.ic_decrypt)
-                        .setContentTitle(title)
-                        .setContentText(subtext);
-        // Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(context, ShowContactMessagesActivity.class);
-        Bundle bundle = new Bundle();
-        bundle.putString(ShowContactMessagesActivity.CONTACT_NUMBER_TO_SHOW, _senderAddress);
-        bundle.putBoolean(ShowContactMessagesActivity.SHOW_ACK_DIALOG, showAckDialog);
-        resultIntent.putExtras(bundle);
-
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-        stackBuilder.addParentStack(ShowContactMessagesActivity.class);
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-        mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        // mId allows you to update the notification later on.
-        mNotificationManager.notify(notifyID, mBuilder.build());
-    }
-
 }
